f6ce8dd80869749c593c4dc5aa362fd203ff5f12
Shreyaas14 <shreyaas.sureshbabu114@gmail.com>
2024-10-08 16:08:10 -0400

    changed port for render

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,7 @@ app.get('*', (req, res) => {
 });
 
 //start server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

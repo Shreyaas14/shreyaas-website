74d4094147e3aa6dacf0639c7f4f5f2841e5e42f
Shreyaas14 <shreyaas.sureshbabu114@gmail.com>
2024-10-08 16:28:13 -0400

    changed desc and logo for site

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,7 @@ app.get('*', (req, res) => {
 });
 
 //start server
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
